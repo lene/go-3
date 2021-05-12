a62f20727f159b569ed395a58eefac94cf49e03a
Lene <lene.preuss@gmail.com>
2021-05-12 19:28:19 +0000

    Scala package and build environment

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import go3d.Color
+
+@main def printDefinitions: Unit = {
+  println("Just some output to verify definitions are ok:")
+  print(Color.Black)
+  print(Color.White)
+  print(Color.Empty)
+  print(Color.Undefined)
+  print(Color.Occupied)
+  print(Color.Red)
+  print(Color.Green)
+  print(Color.Blue)
+  println()
+}
