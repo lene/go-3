93acd3f2d5adee192c0eb2c07055f3f25d59d5b4
Lene Preuss <lene.preuss@gmail.com>
2021-05-22 00:06:06 +0200

    run a full game with random moves and check its running time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,34 +1,32 @@
 import go3d._
 
-def replayGame(goban: Game, moves: List[Move | Pass], delayMs: Int): Unit =
+import scala.util.Random
+import scala.App
+
+def replayGame(goban: Game, moves: List[Move | Pass], delayMs: Int, verbose: Boolean = false): Unit =
   var board = goban
-  for move <- moves do {
+  for move <- moves do
     board = board.makeMove(move)
-    println(move.toString+"\n"+goban)
-    Thread.sleep(delayMs)
+    if verbose then
+      println(s"$move\n$goban")
+      Thread.sleep(delayMs)
+  if !verbose then println(goban)
+
+def randomGame(size: Int): Unit =
+  val random = new Random
+  var game = Game(size)
+  var color = Color.Black
+  while !game.possibleMoves(color).isEmpty && game.moves.length <= size*size*size do
+    val move = Move(game.possibleMoves(color)(random.nextInt(game.possibleMoves(color).length)), color)
+    game = game.makeMove(move)
+    Console.print(s"${game.moves.length} ")
+    Console.flush()
+    color = !color
+  println("\n"+game)
+
+object Runner {
+  def main(args: Array[String]): Unit = {
+    val boardSize = if args.isEmpty then 5 else args(0).toInt
+    randomGame(boardSize)
   }
-
-
-@main def printAGame: Unit =
-  val Delay = 0
-  // watch black capture a white stone
-  val moves1 =  Move(2, 2, 2, Color.Black) :: Move(2, 2, 1, Color.White) ::
-    Move(2, 1, 1, Color.Black) :: Move(2, 2, 3, Color.White) ::
-    Move(2, 3, 1, Color.Black) :: Move(2, 1, 2, Color.White) ::
-    Move(3, 2, 1, Color.Black) :: Move(2, 3, 2, Color.White) ::
-    Move(1, 2, 1, Color.Black) :: Nil
-  replayGame(Game(5), moves1, Delay)
-  Thread.sleep(2*Delay)
-  // black builds an eye, then white captures it
-  val moves2 = List[Move | Pass](
-    Move(2, 1, 1, Color.Black), Pass(Color.White), Move(1, 2, 1, Color.Black), Pass(Color.White),
-    Move(2, 1, 2, Color.Black), Pass(Color.White), Move(1, 2, 2, Color.Black), Pass(Color.White),
-    Move(1, 1, 2, Color.Black),
-    // build the eye first and then encircle it, IMHO that is easier to read
-    Move(1, 3, 1, Color.White), Pass(Color.Black), Move(2, 2, 1, Color.White), Pass(Color.Black),
-    Move(3, 1, 1, Color.White), Pass(Color.Black), Move(1, 3, 2, Color.White), Pass(Color.Black),
-    Move(2, 2, 2, Color.White), Pass(Color.Black), Move(3, 1, 2, Color.White), Pass(Color.Black),
-    Move(1, 1, 3, Color.White), Pass(Color.Black), Move(2, 1, 3, Color.White), Pass(Color.Black),
-    Move(1, 2, 3, Color.White), Pass(Color.Black), Move(1, 1, 1, Color.White)
-  )
-  replayGame(Game(5), moves2, Delay)
+}
