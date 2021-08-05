a09b0a591006f23e8ce73336d241c72b97de3de3
Lene Preuss <lene.preuss@here.com>
2021-08-05 16:54:29 +0200

    factor out some code common to all clients

--- src/main/scala/client/AsciiClient.scala
+++ src/main/scala/client/AsciiClient.scala
@@ -10,31 +10,38 @@ import requests._
 
 class Exit extends RuntimeException
 
-object AsciiClient:
+object AsciiClient extends Client:
 
-  type OptionMap = Map[String, Int|String]
   var gameId: String = ""
   var token: String = ""
-  var client: BaseClient = null
 
   /// sbt "runMain go3d.client.AsciiClient --server $SERVER --port #### --size ## --color [b|w]"
   /// sbt "runMain go3d.client.AsciiClient --server $SERVER --port #### --game-id XXXXXX --color [b|w]"
   /// sbt "runMain go3d.client.AsciiClient --server $SERVER --port #### --game-id XXXXXX --token XXXXX"
-  def main(args: Array[String]): Unit =
-    try
-      parseArgs(args)
-    catch
-      case e: UnknownHostException => exit(s"unknown host: ${e.getMessage}", 1)
-      case e: ConnectException => exit(s"connection problem: ${e.getMessage}", 1)
-      case e: NumberFormatException => exit(s"not a number: ${e.getMessage}", 1)
-      case e: IOException => exit(s"${e.getMessage}", 1)
-      case e: BadColor => exit(s"not a color, must be either black/b/@ or white/w/O", 1)
-      case e: NoSuchElementException => exit(s"missing argument: ${exceptionToParam(e)}", 1)
-      case e: IllegalArgumentException => exit(s"missing argument: ${e.getMessage}", 1)
-    mainLoop(args)
 
-  private def exceptionToParam(e: NoSuchElementException): String =
-    "--" + e.getMessage.substring("key not found: ".length).replace('_', '-')
+  def mainLoop(args: Array[String]): Unit =
+    print(s"server: ${client.serverURL} game: ${client.id} token: ${client.token}  ")
+    val status = waitUntilReady()
+    println(s"\b \n${status.game.goban}")
+    try
+      val input = readLine("your input: ")
+      val Array(command, args) = (input+" ").split("\\s+", 2)
+      command match
+        case "set"|"s" => set(args)
+        case "pass"|"p" => pass
+        case "status"|"st" => getStatus
+        case "exit" =>
+          println("Exiting. If you want to reconnect to the game, enter")
+          println(s"$$ sbt \"runMain go3d.client.AsciiClient --server ${client.serverURL} --game-id ${client.id} --token ${client.token}\"")
+          throw Exit()
+        case _ => println(
+          s"\"$command\" not understood - use \"set|s\", \"pass|p\", \"status|st\" or \"exit\"!"
+        )
+    catch
+      case e: Exit => exit(0)
+      case e: InterruptedException => exit(1)
+      case e: RequestFailedException => println(e)
+    mainLoop(Array())
 
   def parseArgs(args: Array[String]) =
     val options = nextOption(Map(), args.toList)
@@ -77,30 +84,6 @@ object AsciiClient:
           System.exit(1)
           return map
 
-  def mainLoop(args: Array[String]): Unit =
-    print(s"server: ${client.serverURL} game: ${client.id} token: ${client.token}  ")
-    val status = waitUntilReady()
-    println(s"\b \n${status.game.goban}")
-    try
-      val input = readLine("your input: ")
-      val Array(command, args) = (input+" ").split("\\s+", 2)
-      command match
-        case "set"|"s" => set(args)
-        case "pass"|"p" => pass
-        case "status"|"st" => getStatus
-        case "exit" => 
-          println("Exiting. If you want to reconnect to the game, enter")
-          println(s"$$ sbt \"runMain go3d.client.AsciiClient --server ${client.serverURL} --game-id ${client.id} --token ${client.token}\"")
-          throw Exit()
-        case _ => println(
-          s"\"$command\" not understood - use \"set|s\", \"pass|p\", \"status|st\" or \"exit\"!"
-        )
-    catch
-      case e: Exit => exit(0)
-      case e: InterruptedException => exit(1)
-      case e: RequestFailedException => println(e)
-    mainLoop(Array())
-
   def set(args: String): StatusResponse =
     val Array(x, y, z) = args.split("\\s+", 3).map(s => s.trim.toInt)
     println(s"set $x $y $z")
@@ -109,12 +92,7 @@ object AsciiClient:
   def pass: StatusResponse = client.pass
 
   def getStatus: StatusResponse = client.status
-
-  def exit(message: String, status: Int): Unit =
-    if message.length > 0 then println(message)
-    System.exit(status)
-  def exit(status: Int): Unit = exit("", status)
-
+  
   def waitUntilReady(): StatusResponse =
     var status = StatusResponse(null, null, false, null)
     var index = 0
--- src/main/scala/client/StupidClient.scala
+++ src/main/scala/client/StupidClient.scala
@@ -1,7 +1,6 @@
 package go3d.client
 
 import go3d.{BadColor, Black, Color, White}
-import go3d.client.AsciiClient.{OptionMap, client, exit, getStatus, mainLoop, pass, set, waitUntilReady}
 import go3d.client.{BaseClient, Exit}
 import go3d.server.StatusResponse
 
@@ -11,24 +10,33 @@ import java.net.{ConnectException, UnknownHostException}
 import scala.io.StdIn.readLine
 import requests._
 
-object StupidClient:
+object StupidClient extends Client:
 
   val random = Random()
-  def main(args: Array[String]): Unit =
-    try
-      parseArgs(args)
-    catch
-      case e: UnknownHostException => exit(s"unknown host: ${e.getMessage}", 1)
-      case e: ConnectException => exit(s"connection problem: ${e.getMessage}", 1)
-      case e: NumberFormatException => exit(s"not a number: ${e.getMessage}", 1)
-      case e: IOException => exit(s"${e.getMessage}", 1)
-      case e: BadColor => exit(s"not a color, must be either black/b/@ or white/w/O", 1)
-      case e: NoSuchElementException => exit(s"missing argument: ${exceptionToParam(e)}", 1)
-      case e: IllegalArgumentException => exit(s"missing argument: ${e.getMessage}", 1)
-    mainLoop(args)
 
-  private def exceptionToParam(e: NoSuchElementException): String =
-    "--" + e.getMessage.substring("key not found: ".length).replace('_', '-')
+  /// sbt "runMain go3d.client.StupidClient --server $SERVER --port #### --size ## --color [b|w]"
+  /// sbt "runMain go3d.client.StupidClient --server $SERVER --port #### --game-id XXXXXX --color [b|w]"
+  /// sbt "runMain go3d.client.StupidClient --server $SERVER --port #### --game-id XXXXXX --token XXXXX"
+
+  def mainLoop(args: Array[String]): Unit =
+    print(s"server: ${client.serverURL} game: ${client.id} token: ${client.token}  ")
+    val status = waitUntilReady()
+    println(s"\b \n${status.game.goban}")
+    var over = false
+    try
+      val possible = status.moves
+      if possible.nonEmpty then
+        val setPosition = possible(random.nextInt(possible.length))
+        client.set(setPosition.x, setPosition.y, setPosition.z)
+      else
+        over = true
+        client.pass
+    catch
+      case e: Exit => exit(0)
+      case e: InterruptedException => exit(1)
+      case e: RequestFailedException => println(e)
+    if !over then mainLoop(Array())
+    else println(client.status.game)
 
   def parseArgs(args: Array[String]) =
     val options = nextOption(Map(), args.toList)
@@ -70,26 +78,6 @@ object StupidClient:
         println("Unknown option "+option)
         System.exit(1)
         return map
-
-  def mainLoop(args: Array[String]): Unit =
-    print(s"server: ${client.serverURL} game: ${client.id} token: ${client.token}  ")
-    val status = waitUntilReady()
-    println(s"\b \n${status.game.goban}")
-    var over = false
-    try
-      val possible = status.moves
-      if possible.nonEmpty then
-        val setPosition = possible(random.nextInt(possible.length))
-        client.set(setPosition.x, setPosition.y, setPosition.z)
-      else
-        over = true
-        client.pass
-    catch
-      case e: Exit => exit(0)
-      case e: InterruptedException => exit(1)
-      case e: RequestFailedException => println(e)
-    if !over then mainLoop(Array())
-    else println(client.status.game)
 
   def waitUntilReady(): StatusResponse =
     var status = StatusResponse(null, null, false, null)
