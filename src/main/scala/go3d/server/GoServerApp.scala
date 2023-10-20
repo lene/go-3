781d761ab6984b47530ab2405c1c06ce792680b0
Lene Preuss <lene.preuss@gmail.com>
2023-10-20 18:13:47 +0200

    WIP: switch from Jetty to http4s

---
+++ src/main/scala/go3d/server/GoServerApp.scala
@@ -0,0 +1,24 @@
+package go3d.server
+
+import cats.effect._
+import com.comcast.ip4s._
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.ember.server._
+
+object GoServerApp extends IOApp:
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8088")
+      .withHttpApp(helloWorldService)
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
