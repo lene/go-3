e4d07021b08669412ad8fac78da772029487f54b
Lene Preuss <lene.preuss@gmail.com>
2023-11-27 22:43:37 +0100

    create a particle system just because I can (it was hard enough, so let me just show off!)

--- src/main/scala/go3d/client/GDXClient.scala
+++ src/main/scala/go3d/client/GDXClient.scala
@@ -6,12 +6,13 @@ import go3d.client.gdx.GobanDisplay
 
 object GDXClient extends InteractiveClient:
 
-    final val COLOR_BITS = 8
-    final val DEPTH_BITS = 16
-    final val STENCIL_BITS = 0
-    final val NUM_ANTIALIAS_SAMPLES = 4
+    private final val COLOR_BITS = 8
+    private final val DEPTH_BITS = 16
+    private final val STENCIL_BITS = 0
+    private final val NUM_ANTIALIAS_SAMPLES = 4
 
     def mainLoop(args: Array[String]): Unit =
+        println("Starting 3D Go client")
         val config = getConfiguration("3D Go", 1280, 960)
         new Lwjgl3Application(new GobanDisplay(client), config)
 
